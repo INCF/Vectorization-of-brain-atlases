e4e30104aba6739b0583b0322b822b43594d13d6
chiggum <codechiggum@gmail.com>
2014-05-22 20:48:28 +0530

    Completed Step 6 Now working on optimization

--- bitmap2vector/reborder_and_print_final_paths.cpp
+++ bitmap2vector/reborder_and_print_final_paths.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ struct Region
 	int r;
 	int g;
 	int b;
-	Path* closedPath;
+	Path** closedPath;
 };
 
 double cubicBezierValue(double t, double P0, double P1, double P2, double P3)
@@ -94,6 +95,9 @@ Path* reversePath(Path* x)
 		return p;
 	}
 
+	p->start = x->end;
+	p->end = x->start;
+
 	for(int i = 0; i < len; ++i)
 	{
 		if(i == 0)
@@ -139,14 +143,113 @@ Path* reversePath(Path* x)
 	return p;
 }
 
+int getNextPathIndex(int *a, int len, Point b, Path *path_list, int numPaths)
+{
+	int val = 1000;
+	int ind = -1;
+	int i;
+	// 0 OR 1 pixel near check
+	for(i = 0; i < len; ++i)
+	{
+		if(a[i] != -1)
+		{
+			if(a[i] >= numPaths)
+			{
+				cout << "ERROR -----" << endl;
+			}
+			else
+			{
+				int myX = abs(path_list[a[i]].start.x - b.x);
+				int myY = abs(path_list[a[i]].start.y - b.y);
+				if(myX + myY < val)
+				{
+					val = myX + myY;
+					ind = i;
+				}
+
+				myX = abs(path_list[a[i]].end.x - b.x);
+				myY = abs(path_list[a[i]].end.y - b.y);
+				if(myX + myY < val)
+				{
+					val = myX + myY;
+					ind = i;
+				}
+			}
+		}
+	}
+	
+	return ind;
+}
+
+int sameOrRevDirection(Path *path_list, int focus, Point b)
+{
+	int val1;
+	int val2;
+	val1 = abs(path_list[focus].start.x - b.x) + abs(path_list[focus].start.y - b.y);
+	val2 = abs(path_list[focus].end.x - b.x) + abs(path_list[focus].end.y - b.y);
+
+	if(val1 < val2)
+		return 1;
+	else
+		return 0;
+}
+
+void processRegion(Region *region, Path *path_list, int numPaths)
+{
+	int counter = region->numSurroundPaths;
+	int pushed = 0;
+	int focus = 0;
+	int revFlag = 0;
+	while(counter > 0)
+	{
+		if(pushed == 0)
+		{
+			if(region->pathNum[pushed] >= numPaths)
+				cout << "Some Problem" << endl;
+			region->closedPath[pushed] = &path_list[region->pathNum[focus]];
+			region->pathNum[pushed] = -1;  // -1 IMPLIES USED UP PATH
+			counter--;
+			pushed++;	
+			revFlag = 0;		
+		}
+		else
+		{
+			focus = getNextPathIndex(region->pathNum, region->numSurroundPaths, (revFlag == 0)?(region->closedPath[pushed - 1]->end):(region->closedPath[pushed - 1]->reverse->end), path_list, numPaths);
+			if(focus == -1)
+				cout << "ERROR" << endl;
+			else
+			{
+				int temp = region->pathNum[focus];
+				region->pathNum[focus] = -1;
+				focus = temp;
+				
+				if(sameOrRevDirection(path_list, focus, (revFlag == 0)?(region->closedPath[pushed - 1]->end):(region->closedPath[pushed - 1]->reverse->end)))
+				{
+					region->closedPath[pushed] = &path_list[focus];
+					counter--;
+					pushed++;
+					revFlag = 0;
+				}
+				else
+				{
+					region->closedPath[pushed] = path_list[focus].reverse;
+					counter--;
+					pushed++;
+					revFlag = 1;
+				}
+			}
+		}
+	}
+}
+
 int main()
 {
 	int numPaths;
 	int pathLength;
-	Path *myPaths;
+	Path *myPaths = NULL;
 
 	scanf("%d", &numPaths);
-	myPaths = (Path *)malloc(sizeof(numPaths) * sizeof(Path));
+	myPaths = (Path*)malloc(numPaths * sizeof(Path));
 
 	for(int i = 0; i < numPaths; ++i)
 	{	
@@ -158,7 +261,10 @@ int main()
 			myPaths[i].c[j] = (Point*)malloc(sizeof(Point) * 3);
 			for(int k = 0; k < 3; ++k)
 			{
-				scanf("%lf%lf", &myPaths[i].c[j][k].x, &myPaths[i].c[j][k].y);
+				double myX, myY;
+				scanf("%lf%lf", &myX, &myY);
+				myPaths[i].c[j][k].x = myX;
+				myPaths[i].c[j][k].y = myY;
 				if(j == 0 && k == 0)
 				{
 					myPaths[i].start.x = myPaths[i].c[0][0].x;
@@ -177,8 +283,70 @@ int main()
 	}
 
 
+	int numRegions;
+	scanf("%d", &numRegions);
 
+	Region *myRegions = (Region*)malloc(numRegions * sizeof(Region));
 
+	for(int i = 0; i < numRegions; ++i)
+	{
+		int surrPaths;
+		scanf("%d", &surrPaths);
+		myRegions[i].numSurroundPaths = surrPaths;
+		myRegions[i].pathNum = (int*)malloc(surrPaths * sizeof(int));
+		myRegions[i].closedPath = (Path**)malloc(surrPaths * sizeof(Path*));
+
+		for(int j = 0; j < surrPaths; ++j)
+		{
+			int temp;
+			scanf("%d", &temp);
+			myRegions[i].pathNum[j] = temp;
+		}
+		int red, blue, green;
+		scanf("%d%d%d", &red, &green, &blue);
+		myRegions[i].r = red;
+		myRegions[i].g = green;
+		myRegions[i].b = blue;
+
+		processRegion(&myRegions[i], myPaths, numPaths);
+	}
+
+	// Ouput final svg paths
+	for(int i = 0; i < numRegions; ++i)
+	{
+		if(i != 0)
+			cout << "<path  style=\"fill:rgb(" << myRegions[i].r << "," << myRegions[i].g << "," << myRegions[i].b << "); \" d = \"";
+		else
+			cout << "<path fill=\"none\" d =\" ";
+		for(int j = 0; j < myRegions[i].numSurroundPaths; ++j)
+		{
+			for(int k = 0; k < myRegions[i].closedPath[j]->length; ++k)
+			{
+				for(int l = 0; l < 3; ++l)
+				{
+					if(j == 0)
+					{
+						if(k == 0 && l == 0)
+						{
+							cout << "M" << myRegions[i].closedPath[j]->start.x << " " << myRegions[i].closedPath[j]->start.y << " C";
+						}
+					}
+					else
+					{
+						if(k == 0 && l == 0)
+						{
+							cout << "L" << myRegions[i].closedPath[j]->start.x << " " << myRegions[i].closedPath[j]->start.y << " C";
+						}
+					}
+					cout << myRegions[i].closedPath[j]->c[k][l].x << "," << myRegions[i].closedPath[j]->c[k][l].y << " ";
+				}
+			}
+		}
+		cout << " \" />" << endl;
+		cout << endl;
+	}
+
+/*
 
 	for(int i = 0; i < numPaths; ++i)
 	{
@@ -195,7 +363,6 @@ int main()
 		}
 	}
 
-	cout << endl;
 
 	for(int i = 0; i < numPaths; ++i)
 	{
@@ -211,6 +378,10 @@ int main()
 			}
 		}
 	}
+
+
+	*/
+
 
 	return 0;
 }
