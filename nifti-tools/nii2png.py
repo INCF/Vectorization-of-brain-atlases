19b0455a7221e74e40af23f47e58c7677bb13662
Rembrandt Bakker <r.bakker@donders.ru.nl>
2014-06-06 19:05:08 +0200

    Renamed nii2svg to nii2png, and fixed minor bugs. dims = img.shape was before apply_orientation PIL was still used, now eliminated

--- nifti-tools/nii2png.py
+++ nifti-tools/nii2png.py
@@ -1,9 +1,10 @@
-# Rembrandt Bakker, June 2014
+# Rembrandt Bakker, Dhruv Kohli, Piotr Majka, June 2014
 
-import argparse, sys, os, numpy, subprocess
+import argparse, sys, os, numpy, subprocess, json, scipy
 import os.path as op
 import scipy
 import re
+
 
 SliceDirs = {'x':'saggital','y':'coronal','z':'axial'}
 
@@ -15,6 +16,7 @@ def argument_parser():
   parser.add_argument('-i','--nifti_src', type=str, help="Input Nifti file", required=True)
   parser.add_argument('-o','--svg_dest', type=str, help="Output SVG folder", required=True)
   parser.add_argument('-d','--slice_dir', type=str, default='y', help="Slice direction: x=Saggital, y=Coronal, z=Axial")
+  parser.add_argument('-c','--json_colmap', type=str, help="Input JSON colormap file", required=True)
   return parser
 
 def parse_arguments(raw=None):
@@ -26,24 +28,34 @@ def parse_arguments(raw=None):
     print 'Nifti file "{}" not found.'.format(args.nifti_src)
     exit(0)
 
+  if not op.exists(args.json_colmap):
+    print 'JSON colormap file "{}" not found.'.format(args.json_colmap)
+    exit(0)
+
   global SliceDirs
   if SliceDirs[args.slice_dir] is None:
     print 'Invalid slice direction "{}".'.format(args.slice_dir)
     exit(0)
 
   return args
+
+def hex2rgb(v):
+    v = v.lstrip('#')
+    lv = len(v)
+    return tuple(int(v[i:i+lv/3], 16) for i in range(0, lv, lv/3))
            
 def run(args):
   print('Input Nifti: '+args.nifti_src)
+  print('Input JSON colormap file: '+args.json_colmap)
   try:
     import nibabel
     nii = nibabel.load(args.nifti_src)
     img = numpy.squeeze(nii.get_data())
-    dims = img.shape
     hdr = nii.get_header()
     q = hdr.get_best_affine();
     ornt = nibabel.io_orientation(q)
     img = nibabel.apply_orientation(img,ornt)
+    dims = img.shape
     print 'Nifti image loaded, data type "{}"'.format(img.dtype)
 
     global SliceDirs
@@ -58,12 +70,16 @@ def run(args):
     baseName = re.sub('.gz$', '',baseName)
     baseName = re.sub('.nii$', '',baseName)
     pngFolder = op.join(args.svg_dest,'{}_png'.format(slice_dir_orientation))
-    svgFolder = op.join(args.svg_dest,'{}_svg'.format(slice_dir_orientation))
     if not(op.exists(pngFolder)):
       os.makedirs(pngFolder)
-    if not(op.exists(svgFolder)):
-      os.makedirs(svgFolder)
-    print 'Created output folders "{}" and "{}".'.format(pngFolder,svgFolder)
+    print 'Created output folder "{}".'.format(pngFolder)
+
+    with open(args.json_colmap,'r') as fp:
+      colmap = json.load(fp)
+      
+    lookUpTable = {};
+    for a in colmap:
+      lookUpTable[a] = hex2rgb(colmap[a])
 
     if len(dims)==4:
       print 'Error: NIFTI file with RGB color data not supported yet.'
@@ -83,19 +99,24 @@ def run(args):
         slice = img[:,i,::-1].squeeze().transpose();
       elif args.slice_dir is 'z':
         slice = img[:,::-1,i].squeeze().transpose();
-  
-      # Save image to PNG
-      pngFile = baseName+'{:04d}.png'.format(i)
-      scipy.misc.toimage(slice).save(op.join(pngFolder,pngFile))
-      print 'image {} saved to png file "{}".'.format(i,pngFile)
 
-      # Convert image to SVG
       """
-      bmpimage = 'test.bmp'
-      im.save(bmpimage)
-      svgfile = 'test'+str(i)+'.svg'
-      ans = subprocess.call([r"c:\users\rbakker\documents\htdocs\incf\autotrace\autotrace.exe",'-output-file',svgFolder+'/'+svgfile,'-background-color','000000','-corner-surround','0.5','-corner-threshold','180','-corner-always-threshold','180','-error-threshold','0.0','-tangent-surround','1','-filter-iterations','0',bmpimage])
+        In the code below, the indexed image stored in "im" is converted to rgb image which is stored in 
+        2d "finImg" numpy array.
       """
+      shape = slice.shape
+      slice = slice.reshape(-1)
+      rgbImg = numpy.zeros(shape=(slice.shape[0],3), dtype=numpy.uint8)
+      
+      for grayvalue in numpy.unique(slice):
+        mask = (slice == grayvalue)
+        val = lookUpTable[str(grayvalue)]
+        rgbImg[mask] = val
+
+      pngFile = baseName+'_{:04d}.png'.format(i)
+      scipy.misc.toimage(rgbImg.reshape(shape[0],shape[1],3)).save(op.join(pngFolder,pngFile))
+      print 'image {} saved to png file "{}".'.format(i,pngFile)
+      
   except:
     print "Unexpected error:", sys.exc_info()[0]
     raise
