d6541d712816afddd30f630bc2b43afa17b0cffc
chiggum <codechiggum@gmail.com>
2014-06-10 03:26:44 +0530

    clean up

--- nifti-tools/b2v.sh
+++ nifti-tools/b2v.sh
@@ -1,14 +1,16 @@
+ulimit -s unlimited
+
 if [ $# -eq 0 ]
   then
-    echo "Usage: /b2v.sh path_to_filename.png [path_to_outputfileName.svg]"
+    echo "[for help]:./b2v -h"
 fi
 
 if [ $# -eq 1 ]
   then
-    $(dirname "$(readlink -f "$0")")/b2v $1
+    $(dirname "$(readlink -f "$0")")/bin/b2v $1
 fi
 
 if [ $# -eq 2 ]
   then
-    $(dirname "$(readlink -f "$0")")/b2v $1 $2
+    $(dirname "$(readlink -f "$0")")/bin/b2v $1 $2
 fi
