be18644882c818ec1cdbd2fa2caf3b8c217a340f
chiggum <codechiggum@gmail.com>
2014-06-10 04:06:01 +0530

    hex color coding and no empty paths

--- b2v/src/util.cpp
+++ b2v/src/util.cpp
@@ -1,6 +1,8 @@
 #include "util.h"
 #include "lodepng.h"
 #include <iostream>
+#include <cstdio>
+#include <string>
 
 int ifEqualPixel(pixel x, pixel y)
 {
@@ -18,4 +20,25 @@ void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsi
 
   //if there's an error, display it
   if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+}
+
+std::string RGBToHex(uint rNum, uint gNum, uint bNum)
+{
+    std::string result;
+
+    result.append("#");
+
+    char r[255];   
+    sprintf(r, "%.2X", rNum);
+    result.append(r );
+
+    char g[255];   
+    sprintf(g, "%.2X", gNum);
+    result.append(g );
+
+    char b[255];   
+    sprintf(b, "%.2X", bNum);
+    result.append(b ); 
+
+    return result;
 }
