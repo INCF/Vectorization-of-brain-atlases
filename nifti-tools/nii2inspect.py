f8c6adae0ce4c149febaa336af109fc399673077
Rembrandt Bakker <r.bakker@donders.ru.nl>
2014-06-22 22:50:22 +0200

    New arguments for nii2inspect; can now specify color range

--- nifti-tools/nii2inspect.py
+++ nifti-tools/nii2inspect.py
@@ -4,6 +4,7 @@ import argparse, sys, os, numpy, subprocess
 import os.path as op
 import scipy
 import re
+import json
 
 SliceDirs = {'x':'Left-Right','y':'Posterior-Anterior','z':'Inferior-Superior'}
 
@@ -12,19 +13,11 @@ def argument_parser():
   parser = argparse.ArgumentParser(
     description='description',
     formatter_class=argparse.RawTextHelpFormatter)
-  parser.add_argument('-i','--nifti_lr1', type=str, help="Input Nifti file layer 1", required=True)
-  parser.add_argument('-j','--nifti_lr2', type=str, help="Input Nifti file layer 2", required=False)
-  parser.add_argument('-k','--nifti_lr3', type=str, help="Input Nifti file layer 3", required=False)
+  parser.add_argument('-i','--nifti_json', nargs='+', type=json.loads, help="Input Nifti json struct with fields 'file','title' and 'colormap'")
   parser.add_argument('-o','--path_dest', type=str, help="Output snapshot folder", required=True)
-  parser.add_argument('-c','--colormap_lr1', type=str, help="JSON colormap layer 1", required=False)
-  parser.add_argument('-d','--colormap_lr2', type=str, help="JSON colormap layer 2", required=False)
-  parser.add_argument('-e','--colormap_lr3', type=str, help="JSON colormap layer 3", required=False)
   parser.add_argument('-sx','--slices_x', type=str, help="Slices in the x-dimension, start%:step%:stop%", required=False)
   parser.add_argument('-sy','--slices_y', type=str, help="Slices in the y-dimension, start%:step%:stop%", required=False)
   parser.add_argument('-sz','--slices_z', type=str, help="Slices in the z-dimension, start%:step%:stop%", required=False)
-  parser.add_argument('-t1','--title_lr1', type=str, help="Title layer 1", required=False)
-  parser.add_argument('-t2','--title_lr2', type=str, help="Title layer 2", required=False)
-  parser.add_argument('-t3','--title_lr3', type=str, help="Title layer 3", required=False)
   return parser
 
 def parse_arguments(raw=None):
@@ -32,18 +25,13 @@ def parse_arguments(raw=None):
     args = argument_parser().parse_args(raw)
 
     try:
+        print "nifti_json {}".format(args.nifti_json)
+
         """ Basic argument checking """
-        if not args.nifti_lr1:
-            raise Exception('Nifti file for layer 1 must be specified')
-      
-        if args.nifti_lr1 and not op.exists(args.nifti_lr1):
-            raise Exception('Nifti file "{}" not found.'.format(args.nifti_lr1))
-
-        if args.nifti_lr2 and not op.exists(args.nifti_lr2):
-            raise Exception('Nifti file "{}" not found.'.format(args.nifti_lr2))
-
-        if args.nifti_lr3 and not op.exists(args.nifti_lr3):
-            raise Exception('Nifti file "{}" not found.'.format(args.nifti_lr3))
+        args.layers = args.nifti_json
+        for lr in args.layers:
+            if not op.exists(lr["file"]):
+                raise Exception('Nifti file "{}" not found.'.format(lr["file"]))
 
         args.sliceRangePct = [[],[],[]]
         for d in [0,1,2]:
@@ -81,9 +69,7 @@ def hex2rgba(v):
     return tuple(rgba)
 
 def run(args):
-    print('Input Nifti layer 1: {}'.format(args.nifti_lr1))
-    print('Input Nifti layer 2: {}'.format(args.nifti_lr2))
-    print('Input Nifti layer 3: {}'.format(args.nifti_lr3))
+    print('Layer specification: {}'.format(args.layers))
     try:
         destFolder = args.path_dest
         if not(op.exists(destFolder)):
@@ -92,49 +78,63 @@ def run(args):
         destFolder = op.realpath(destFolder)
         scriptDir = op.realpath(op.dirname(__file__))
 
-        redirect = {}
-        for mode in ['lr1','lr2','lr3']:
-            nifti_src = getattr(args,'nifti_'+mode);
+        parsedLayers = []
+        for i,lr in enumerate(args.layers):
+            nifti_src = lr["file"]
             if not nifti_src: continue
-            colormap = getattr(args,'colormap_'+mode);
+            baseName = re.sub('(\.nii|\.nii.gz)$','',op.basename(nifti_src))
 
             import nibabel
             nii = nibabel.load(nifti_src)
             img = numpy.squeeze(nii.get_data())
-            print 'Image type: {} {}-{}'.format(img.dtype,numpy.amin(img),numpy.amax(img))
+            img_min = numpy.amin(img)
+            img_max = numpy.amax(img)
+            print 'Image type: {} {}-{}'.format(img.dtype,img_min,img_max)
             hdr = nii.get_header()
             q = hdr.get_best_affine();
             ornt = nibabel.io_orientation(q)
             print 'Orientation: {}'.format(ornt)
             img = nibabel.apply_orientation(img,ornt)
             dims = img.shape
-            print 'Nifti image for layer {} loaded, data type "{}"'.format(mode,img.dtype)
+            print 'Nifti image for layer {} loaded, data type "{}"'.format(i,img.dtype)
 
             if len(dims)==4:
-                print 'Error: NIFTI file with RGB color data not supported yet.'
+                print 'Error: NIFTI file with RGB color data not supported.'
                 exit(0)
-
-            baseName = op.basename(nifti_src).rstrip('.gz').rstrip('.nii')
-            if colormap: fmt = 'png' 
-            else: fmt = 'jpg'
-            redirect[mode] = baseName+'.'+fmt;
-
+            
+            # apply colormap
+            fmt = 'jpg'
             index2rgb = None
-            if colormap:
-                if colormap[0] is '#':
-                    colormap = colormap.split(',')
-                    colormap = dict((str(i),val) for i,val in enumerate(colormap))
+            mpc_cmap = None
+            if "colormap" in lr:
+                cmap = lr["colormap"]
+                matches = re.search('^(#[0-9a-fA-F]+)-(#[0-9a-fA-F]+)$',cmap)
+                if matches:
+                    import matplotlib.colors as mpc
+                    c1 = mpc.hex2color(matches.group(1))
+                    c2 = mpc.hex2color(matches.group(2))
+                    mpc_cmap = mpc.LinearSegmentedColormap(baseName,{
+                        'red':[(0.0,c1[0],c1[0]),(1.0,c2[0],c2[0])],
+                        'green':[(0.0,c1[1],c1[1]),(1.0,c2[1],c2[1])],
+                        'blue':[(0.0,c1[2],c1[2]),(1.0,c2[2],c2[2])],
+                    })
+                    #print('mpc_cmap {}'.format(mpc_cmap(numpy.arange(256))))
                 else:
-                    if not op.exists(colormap):
-                        raise('JSON colormap file "{}" not found.'.format(colormap))
+                    fmt = 'png'
+                    if cmap[0] is '#':
+                        cmap = cmap.split(',')
+                        cmap = dict((str(i),val) for i,val in enumerate(cmap))
+                    else:
+                        if not op.exists(cmap):
+                            raise('JSON colormap file "{}" not found.'.format(cmap))
+                      
+                        with open(cmap,'r') as fp:
+                            cmap = json.load(fp)
                   
-                    with open(colormap,'r') as fp:
-                        colormap = json.load(fp)
-                  
-                index2rgb = {};
-                for a in colormap:
-                    index2rgb[a] = hex2rgba(colormap[a])
-                print "Color map {}".format(index2rgb)
+                    index2rgb = {};
+                    for a in cmap:
+                        index2rgb[a] = hex2rgba(cmap[a])
+                    print "Color map {}".format(index2rgb)
 
             sliceRange = [[],[],[]]
             for d in [0,1,2]:
@@ -146,58 +146,77 @@ def run(args):
                 sliceRange[d] = [sliceStart,sliceStep,sliceEnd]
                 for i in range(sliceStart,sliceEnd+1,sliceStep):
                     slice = get_slice(img,dim,i)
-                    if index2rgb:
-                        shape = slice.shape
-                        slice = slice.reshape(-1)
-                        rgbImg = numpy.zeros(shape=(slice.shape[0],4), dtype=numpy.uint8)
-                        for grayvalue in numpy.unique(slice):
-                            mask = (slice == grayvalue)
-                            val = index2rgb[str(grayvalue)]
-                            rgbImg[mask] = val
-                        slice = rgbImg.reshape(shape[0],shape[1],4)
+                    
+                    pngFile = baseName+'_{}{:d}.{}'.format(dim,i,fmt)
+                    if mpc_cmap:
+                        import matplotlib as mpl
+                        import matplotlib.cm as cm
+                        norm = mpl.colors.Normalize(vmin=img_min, vmax=img_max)
+                        m = cm.ScalarMappable(norm=norm, cmap=mpc_cmap)
+                        slice = m.to_rgba(slice)
+                        #import Image
+                        #slice = (slice-img_min)/(img_max-img_min)
+                        #if i==0: print 'A {}'.format(numpy.uint8(mpc_cmap(slice)*255))
+                        #im = Image.fromarray(numpy.uint8(mpc_cmap(slice)*255))
+                        #im.save(pngFile)
+                        
+                        #print 'VIA MPC_MAP'
+
+                    else:
+                        if index2rgb:
+                            shape = slice.shape
+                            slice = slice.reshape(-1)
+                            rgbImg = numpy.zeros(shape=(slice.shape[0],4), dtype=numpy.uint8)
+                            for grayvalue in numpy.unique(slice):
+                                mask = (slice == grayvalue)
+                                val = index2rgb[str(grayvalue)]
+                                rgbImg[mask] = val
+                            slice = rgbImg.reshape(shape[0],shape[1],4)
 
                     # Save image to PNG
-                    #pngFile = baseName+'_{}{:02d}.{}'.format(dim,100*(sample+1)/(numSamples+1),fmt)
-                    pngFile = baseName+'_{}{:d}.{}'.format(dim,i,fmt)
                     scipy.misc.toimage(slice).save(op.join(destFolder,pngFile))
+                        
                     if i==sliceStart:
                         print 'image {}{} saved to png file "{}".'.format(dim,i,pngFile)
+
+            # update parsedLayers
+            pl = {
+              "name": baseName,
+              "ext": fmt
+            }
+            if "title" in lr:
+                pl["title"] = lr["title"];
+            parsedLayers.append(pl);
 
         inspectFile = '{}/nii_inspect.html'.format(scriptDir);
         with open(inspectFile, 'r') as fp:
             html = fp.read()
-            for lr in [1,2,3]:
-                try:
-                    nifti_src = getattr(args,'nifti_lr{}'.format(lr));
-                    if nifti_src is not None:
-                        html = html.replace(r"/*$ if (!lr{}) lr{} = '' $*/".format(lr,lr),
-                            r"if (!lr{}) lr{} = '{}';".format(lr,lr,redirect['lr{}'.format(lr)]))
-                    title = getattr(args,'title_lr{}'.format(lr))
-                    if title is not None:
-                        html = html.replace(r"/*$ if (!ti{}) ti{} = 'layer{}' $*/".format(lr,lr,lr),
-                            r"if (!ti{}) ti{} = '{}';".format(lr,lr,title))
-                        
-                except NameError:
-                    raise
+            html = html.replace(r"var defaultLayers = [];",
+                r"var defaultLayers = {};".format(json.dumps(parsedLayers)))
+                            
+            #for lr in [1,2,3]:
+            #    try:
+            #        nifti_src = getattr(args,'nifti_lr{}'.format(lr));
+            #        if nifti_src is not None:
+            #            
+            #            html = html.replace(r"/*$ if (!lr{}) lr{} = '' $*/".format(lr,lr),
+            #                r"if (!lr{}) lr{} = '{}';".format(lr,lr,redirect['lr{}'.format(lr)]))
+            #        title = getattr(args,'title_lr{}'.format(lr))
+            #        if title is not None:
+            #            html = html.replace(r"/*$ if (!ti{}) ti{} = 'layer{}' $*/".format(lr,lr,lr),
+            #                r"if (!ti{}) ti{} = '{}';".format(lr,lr,title))
+            #            
+            #    except NameError:
+            #        raise
                     
-            for d in [0,1,2]:
-                dim = ['X','Y','Z'][d]
-                html = html.replace("/*$ if (!slices{}) slices{} = '0:1:2' $*/".format(dim,dim),
-                    "if (!slices{}) slices{} = '{}';".format(dim,dim,':'.join(str(s) for s in sliceRange[d])))
+            html = html.replace(r"var defaultSliceRange = [];",
+                "var defaultSliceRange = {};".format(json.dumps(sliceRange)))
   
         htmlFile = op.join(destFolder,'index.html')
         with open(htmlFile, 'w') as fp:
             fp.write(html)
                 
         print 'HTML viewer saved as "{}"'.format(htmlFile)
-
-        """
-        try:
-          os.symlink(op.join(op.dirname(__file__),'snapshot.html'),op.join(destFolder,'snapshot.html'))
-        except:
-          import shutil
-          shutil.copyfile(op.join(op.dirname(__file__),'snapshot.html'),op.join(destFolder,'snapshot.html'))
-        """
         
     except:
         print "Unexpected error:", sys.exc_info()[0]
